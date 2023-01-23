eb51a89e025ab9c662c06d187d087c86f79b4e5a
Atila jr <atilla.junior33@gmail.com>
2023-01-23 14:05:18 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/atillaJrWorkshopmongo/resources/UserResource.java
+++ src/main/java/com/atillaJrWorkshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.atillaJrWorkshopmongo.resources;
 
+import com.atillaJrWorkshopmongo.domain.Post;
 import com.atillaJrWorkshopmongo.domain.User;
 import com.atillaJrWorkshopmongo.dto.UserDTO;
 import com.atillaJrWorkshopmongo.services.UserService;
@@ -70,5 +71,11 @@ public class UserResource {
     obj.setId(id);
     obj = service.update(obj);
     return ResponseEntity.noContent().build();
+  }
+
+  @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+  public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+    User obj = service.findById(id);
+    return ResponseEntity.ok().body(obj.getPots());
   }
 }
