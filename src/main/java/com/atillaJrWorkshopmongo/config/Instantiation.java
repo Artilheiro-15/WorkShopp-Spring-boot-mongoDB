bd2e082d5e2026178a2d42be028ff2f9972d6b50
Atila jr <atilla.junior33@gmail.com>
2023-01-23 13:20:43 -0300

    Criando entity Post com user aninhado

--- src/main/java/com/atillaJrWorkshopmongo/config/Instantiation.java
+++ src/main/java/com/atillaJrWorkshopmongo/config/Instantiation.java
@@ -1,8 +1,12 @@
 package com.atillaJrWorkshopmongo.config;
 
+import com.atillaJrWorkshopmongo.domain.Post;
 import com.atillaJrWorkshopmongo.domain.User;
+import com.atillaJrWorkshopmongo.repository.PostRepository;
 import com.atillaJrWorkshopmongo.repository.UserRepository;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -13,17 +17,40 @@ public class Instantiation implements CommandLineRunner {
   @Autowired
   private UserRepository userRepository;
 
+  @Autowired
+  private PostRepository postRepository;
+
   @Override
   public void run(String... args) throws Exception {
-    //com isso aqui eu removo os usuarios do banco de dados
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+    //com isso aqui eu Limpo os usuarios do banco de dados
     userRepository.deleteAll();
+    postRepository.deleteAll();
 
     User Atilla = new User(null, "Atilla Brown", "Atilla@gmail.com");
     User maria = new User(null, "Maria Brown", "maria@gmail.com");
     User alex = new User(null, "Alex Green", "alex@gmail.com");
     User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+    Post post1 = new Post(
+      null,
+      sdf.parse("21/03/2023"),
+      "Partiu viagem",
+      "Vou viajar para Sao Paulo. Abraços! ",
+      maria
+    );
+    Post post2 = new Post(
+      null,
+      sdf.parse("23/03/2023"),
+      "Bom Dia ",
+      "Acordei feliz Hoje!",
+      maria
+    );
+
     //Com isso aqui eu salvo vo salvar os usuarios automaticamente la no banco de dados
     userRepository.saveAll(Arrays.asList(Atilla, maria, alex));
+    postRepository.saveAll(Arrays.asList(post1, post2));
   }
 }
---
+++ src/main/java/com/atillaJrWorkshopmongo/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.atillaJrWorkshopmongo.repository;
+
+import com.atillaJrWorkshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {}
