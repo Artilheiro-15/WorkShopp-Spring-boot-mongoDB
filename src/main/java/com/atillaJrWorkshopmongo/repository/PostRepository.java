d88cfeeb428ac36e6f249981e1d85d0683b2c100
Atila jr <atilla.junior33@gmail.com>
2023-01-23 16:45:46 -0300

    Consulta simples com @Query

--- src/main/java/com/atillaJrWorkshopmongo/repository/PostRepository.java
+++ src/main/java/com/atillaJrWorkshopmongo/repository/PostRepository.java
@@ -3,9 +3,13 @@ package com.atillaJrWorkshopmongo.repository;
 import com.atillaJrWorkshopmongo.domain.Post;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+  @Query("{'title': { $regex: ?0, $options: 'i' } }")
+  List<Post> searchTitle(String text);
+
   List<Post> findByTitleContainingIgnoreCase(String text);
 }
